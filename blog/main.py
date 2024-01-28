cf008b11d4b25a5f82eef9c605802bd7368ccd1a
DatTran <trancongdat.datct@gmail.com>
2024-01-28 16:43:33 +0700

    add respone model

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,7 @@ from blog import schemas
 from . import models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from typing import List
 
 app = FastAPI()
 
@@ -23,7 +24,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get("/blog")
+@app.get("/blog", status_code=200, response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
@@ -37,7 +38,7 @@ def destroy(id, db: Session = Depends(get_db)):
     db.commit()
     return "done"
 
-@app.get("/blog/{id}", status_code=200)
+@app.get("/blog/{id}", status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response ,db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
